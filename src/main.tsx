78f262883fb766c774b73ef0d54983263a5bfef2
Rene Rubalcava <rrubalcava@esri.com>
2020-11-24 09:07:51 -0800

    remove comments

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,5 @@
 import renderer, { tsx } from '@dojo/framework/core/vdom';
-// import Registry from '@dojo/framework/core/Registry';
 import App from './App';
-
-// const registry = new Registry();
 
 const r = renderer(() => <App />);
 r.mount();
